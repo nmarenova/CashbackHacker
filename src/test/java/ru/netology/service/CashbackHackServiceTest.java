f4c673ffb2e0db6b3ea122b10de50614f957db7e
Natalia Marenova <nmarenova@yandex.ru>
2021-02-24 20:44:44 +0300

    Add branch junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,35 +1,37 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void shouldCalculateIfAmountCorrect() {
-        CashbackHackService service= new CashbackHackService();
-        int amount = 2500;
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
+
 
     @Test
     public void shouldCalculateIfAmountZero() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void shouldCalculateIfAmount1000() {
-        CashbackHackService service= new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected, actual);
     }
 
 }
