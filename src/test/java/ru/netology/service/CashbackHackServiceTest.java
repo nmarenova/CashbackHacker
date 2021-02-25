759bb6702c1942e6b1e4889bad036f30045a514b
Natalia Marenova <nmarenova@yandex.ru>
2021-02-25 20:34:34 +0300

    Code duplication removing

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,20 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCalculateIfAmountCorrect() {
-        CashbackHackService service = new CashbackHackService();
         int amount = 3500;
         int actual = service.remain(amount);
         int expected = 500;
         assertEquals(expected, actual);
     }
 
-
     @Test
     public void shouldCalculateIfAmountZero() {
-        CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
@@ -27,11 +25,9 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldCalculateIfAmount1000() {
-        CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
         assertEquals(expected, actual);
     }
-
 }
